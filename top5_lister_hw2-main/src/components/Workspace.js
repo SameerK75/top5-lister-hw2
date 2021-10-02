3fc62c604467b9fee685cb061218187b79a5c595
SameerK75 <sameerkmun@gmail.com>
2021-10-02 18:14:14 -0400

    load list items

--- top5_lister_hw2-main/src/components/Workspace.js
+++ top5_lister_hw2-main/src/components/Workspace.js
@@ -2,6 +2,11 @@ import React from "react";
 
 export default class Workspace extends React.Component {
     render() {
+        const {currentList} = this.props;
+        let items = [];
+        if (currentList !== null) {
+            items = currentList.items;
+        }
         return (
             <div id="top5-workspace">
                 <div id="workspace-edit">
@@ -11,6 +16,13 @@ export default class Workspace extends React.Component {
                         <div className="item-number">3.</div>
                         <div className="item-number">4.</div>
                         <div className="item-number">5.</div>
+                    </div>
+                    <div id = "edit-items">
+                        <div id='item-1' class="top5-item"> {items[0]}</div>
+                        <div id='item-2' class="top5-item"> {items[1]}</div>
+                        <div id='item-3' class="top5-item"> {items[2]}</div>
+                        <div id='item-4' class="top5-item"> {items[3]}</div>
+                        <div id='item-5' class="top5-item"> {items[4]}</div>
                     </div>
                 </div>
             </div>
